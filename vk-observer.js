887fd8335f1e6234436024df0122adc4dab79598
Glebcha <glebcha@gmail.com>
2014-04-08 20:06:46 +0700

    added artist name

--- vk-observer.js
+++ vk-observer.js
@@ -8,15 +8,17 @@ var vkMusic = {
                 if (!btn.querySelector('.download-link')) {
                     var getLink = btn.parentNode.lastElementChild.value.split('?').splice(0, 1).toString();
                     var audioTitle = audioBlock.querySelector('.title_wrap.fl_l .title').innerText;
+                    var audioArtist = audioBlock.querySelector('.title_wrap.fl_l').firstElementChild.firstElementChild.innerText;
+                    var audioFullName  = audioArtist + "-" + audioTitle;
                     var audioDurationBlock = audioBlock.querySelector('.duration')
                     var audioDurationText = audioDurationBlock.innerText.split(':');
                     var audioDurationSeconds = audioDurationText[0] * 60 + +audioDurationText[1];
                     audioDurationBlock.setAttribute('data-duration', audioDurationSeconds);
                     var d = document.createElement('a');
-                    var downloadData = 'audio/mpeg3:' + audioTitle + '.mp3:' + getLink;
+                    var downloadData = 'audio/mpeg:' + audioFullName + '.mp3:' + getLink;
                     d.className = 'download-link';
                     d.href = getLink;
-                    d.setAttribute('download', audioTitle);
+                    d.setAttribute('download', audioFullName);
                     d.setAttribute('data-download', downloadData);
                     d.addEventListener('click',
                         function (event) {
