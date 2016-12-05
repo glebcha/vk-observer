a3da50130a4e655079f829d0131ce08289c1ca4a
Glebcha <glebcha@gmail.com>
2016-12-06 00:18:29 +0700

    changed top player classname/commented scrobbler

--- source/js/app/watcher.js
+++ source/js/app/watcher.js
@@ -49,7 +49,7 @@ class pageMedia {
                         playlist = node.querySelector('.audio_playlist_wrap'),
                         v = node.querySelector('#mv_layer_wrap'),//video modal
                         m = node.querySelector('#wk_layer_wrap '),//wall post modal
-                        ticker = node.querySelector('.top_audio_player');
+                        ticker = node.querySelector('.eltt top_audio_layer');
 
                     if (v) {
 
@@ -93,93 +93,89 @@ class pageMedia {
                     }
 
                     if (ticker) {
-                        let lastContainer = ticker
-                                            .parentNode
-                                            .querySelector('.last-controls'),
-                            iconL = ticker
-                                    .parentNode
-                                    .querySelector('#like-icon'),
-                            fullTitle = ticker
-                                        .querySelector('.top_audio_player_title')
-                                        .innerText
-                                        .replace(/\s?([–-])\s?/g, '-')
-                                        .split('-');
-
-                        setTimeout( () => {
-                            localStorage.vkObserver_artist = fullTitle[0];
-                            localStorage.vkObserver_title = fullTitle[1];
-                        }, 2000);
-
-                        if(!lastContainer){
-                            let lastControls = document.createElement('div'),
-                                scrobbleIcon = document.createElement('div'),
-                                likeIcon = document.createElement('div');
-                            lastControls.className = 'last-controls';
-                            scrobbleIcon.id = 'scrobble-icon';
-                            likeIcon.id = 'like-icon';
-                            lastControls.appendChild(scrobbleIcon);
-                            lastControls.appendChild(likeIcon);
-                            ticker.appendChild(lastControls);
-                        }
-
-                        ticker
-                        .parentNode
-                        .querySelector('#like-icon')
-                        .onclick = (e) => {
-                            e.stopPropagation();
-                            scrobbler.likeSong(
-                              localStorage.vkObserver_artist,
-                              localStorage.vkObserver_title,
-                              iconL
-                            );
-                        };
+                        // let lastContainer = ticker
+                        //                     .parentNode
+                        //                     .querySelector('.last-controls'),
+                        //     iconL = ticker
+                        //             .parentNode
+                        //             .querySelector('#like-icon'),
+                        //     fullTitle = ticker
+                        //                 .querySelector('.top_audio_player_title')
+                        //                 .innerText
+                        //                 .replace(/\s?([–-])\s?/g, '-')
+                        //                 .split('-');
+                        //
+                        // setTimeout( () => {
+                        //     localStorage.vkObserver_artist = fullTitle[0];
+                        //     localStorage.vkObserver_title = fullTitle[1];
+                        // }, 2000);
+                        //
+                        // if(!lastContainer){
+                        //     let lastControls = document.createElement('div'),
+                        //         scrobbleIcon = document.createElement('div'),
+                        //         likeIcon = document.createElement('div');
+                        //     lastControls.className = 'last-controls';
+                        //     scrobbleIcon.id = 'scrobble-icon';
+                        //     likeIcon.id = 'like-icon';
+                        //     lastControls.appendChild(scrobbleIcon);
+                        //     lastControls.appendChild(likeIcon);
+                        //     ticker.appendChild(lastControls);
+                        // }
+                        //
+                        // ticker
+                        // .parentNode
+                        // .querySelector('#like-icon')
+                        // .onclick = (e) => {
+                        //     e.stopPropagation();
+                        //     scrobbler.likeSong(
+                        //       localStorage.vkObserver_artist,
+                        //       localStorage.vkObserver_title,
+                        //       iconL
+                        //     );
+                        // };
 
                         let tickerObs = new MutationObserver(
                           (mutations, observer) => {
                             mutations.forEach( (mutation) => {
-                                const playing = mutation.target,
-                                        fullTitle = playing
-                                                    .parentNode
-                                                    .parentNode
-                                                    .querySelector('.top_audio_player_title');
-                                const  formattedTitle = fullTitle
-                                                        .innerText
-                                                        .replace(/\s?([–-])\s?/g, '-')
-                                                        .split('-');
-                                const artist = formattedTitle[0],
-                                        title = formattedTitle[1],
-                                        iconStatus = ticker
-                                                    .parentNode
-                                                    .querySelector('#scrobble-icon'),
-                                        iconLike = ticker
-                                                    .parentNode
-                                                    .querySelector('#like-icon');
-
-                                if (localStorage.VkObserver_scrobble !== 'disabled'){
-                                    iconStatus.style.visibility = 'visible';
-                                    iconLike.style.visibility = 'visible';
-                                } else {
-                                    iconStatus.style.visibility = 'hidden';
-                                    iconLike.style.visibility = 'hidden';
-                                }
-
-                                if (fullTitle && title !== localStorage.vkObserver_title) {
-                                    window.clearTimeout(checker);
-                                    iconStatus.className = '';
-                                    iconStatus.setAttribute('title', 'скробблится');
-                                    iconLike.className = 'changed';
-                                    iconLike.setAttribute('title', 'добавить в любимые');
-                                    localStorage.vkObserver_title = title;
-                                    localStorage.vkObserver_artist = artist;
-                                    checker = window.setTimeout(() => {
-                                        scrobbler.scrobble(
-                                          localStorage.vkObserver_artist,
-                                          localStorage.vkObserver_title,
-                                          iconStatus
-                                        );
-                                        iconLike.className = '';
-                                    }, 21000);
-                                }
+                                const playing = mutation.target;
+                                const artist = playing
+                                                .querySelector('.audio_page_player_title_performer')
+                                                .innerHTML;
+                                const title = playing
+                                                .querySelector('.audio_page_player_title')
+                                                .innerHTML;
+                                // const iconStatus = ticker
+                                //                     .parentNode
+                                //                     .querySelector('#scrobble-icon');
+                                // const iconLike = ticker
+                                //                     .parentNode
+                                //                     .querySelector('#like-icon');
+                                //
+                                // if (localStorage.VkObserver_scrobble !== 'disabled'){
+                                //     iconStatus.style.visibility = 'visible';
+                                //     iconLike.style.visibility = 'visible';
+                                // } else {
+                                //     iconStatus.style.visibility = 'hidden';
+                                //     iconLike.style.visibility = 'hidden';
+                                // }
+                                //
+                                // if (fullTitle && title !== localStorage.vkObserver_title) {
+                                //     window.clearTimeout(checker);
+                                //     iconStatus.className = '';
+                                //     iconStatus.setAttribute('title', 'скробблится');
+                                //     iconLike.className = 'changed';
+                                //     iconLike.setAttribute('title', 'добавить в любимые');
+                                //     localStorage.vkObserver_title = title;
+                                //     localStorage.vkObserver_artist = artist;
+                                //     checker = window.setTimeout(() => {
+                                //         scrobbler.scrobble(
+                                //           localStorage.vkObserver_artist,
+                                //           localStorage.vkObserver_title,
+                                //           iconStatus
+                                //         );
+                                //         iconLike.className = '';
+                                //     }, 21000);
+                                // }
 
                             })
                         });
