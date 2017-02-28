a4922441960c59d0d73c53149fad2cd218fa23ad
Glebcha <glebcha@gmail.com>
2017-03-01 00:57:32 +0700

    will use later

--- source/js/pages/background.js
+++ source/js/pages/background.js
@@ -1,26 +1,26 @@
-let tabId;
-
-chrome.runtime.onMessage.addListener(
-  (request, sender, sendResponse) => {
-    if (request.query === 'getvideo') {
-    	chrome.downloads.download({
-            url: request.url,
-            filename: request.name + '.mp4'
-        }, (id) => {
-        	tabId = sender.tab.id;
-        	//console.info("started video " + id, tabId);
-        	sendResponse({message: "started '" + request.name + "'' video with id" + id});
-        });
-        return true;
-    }
-});
-
-chrome.downloads.onChanged.addListener((details) => {
-   	if(details.state && details.state.current == "complete"){
-  		chrome.tabs.sendMessage(
-  		tabId,
-  		{query: 'downloadvideo', details: details}, (response) => {
-    		//console.log(response);
-  		});
-   	}
-});
+// let tabId;
+//
+// chrome.runtime.onMessage.addListener(
+//   (request, sender, sendResponse) => {
+//     if (request.query === 'getvideo') {
+//     	chrome.downloads.download({
+//             url: request.url,
+//             filename: request.name + '.mp4'
+//         }, (id) => {
+//         	tabId = sender.tab.id;
+//         	//console.info("started video " + id, tabId);
+//         	sendResponse({message: "started '" + request.name + "'' video with id" + id});
+//         });
+//         return true;
+//     }
+// });
+//
+// chrome.downloads.onChanged.addListener((details) => {
+//    	if(details.state && details.state.current == "complete"){
+//   		chrome.tabs.sendMessage(
+//   		tabId,
+//   		{query: 'downloadvideo', details: details}, (response) => {
+//     		//console.log(response);
+//   		});
+//    	}
+// });
