1c5ff652e0b88ad5e16d19735ae7565a1f6313f8
Glebcha <glebcha@gmail.com>
2017-08-26 01:16:08 +0700

    performance optimizations

--- source/js/app/audio.js
+++ source/js/app/audio.js
@@ -59,11 +59,9 @@ class Audio extends vkObserver {
 
 	}
 
-	displayBitrate(e, options) {
+	displayBitrate(target, options) {
 		const { url, id, title, duration } = options;
 		const bitrateStatus = localStorage.VkObserver_bitrate;
-
-		e.preventDefault();
 
 		xhr({
 			url,
@@ -81,9 +79,8 @@ class Audio extends vkObserver {
 		})
 		.then(data => {
 			const { fileSize, bitrate } = data.optional
-			const audioContainer = e.target;
 
-			if (bitrateStatus === 'enabled' && !audioContainer.querySelector('.bitrate')) {
+			if (bitrateStatus === 'enabled' && !target.querySelector('.bitrate')) {
 				let text;
 				if (isNaN(bitrate.kbps) === true) {
 					text = '×';
@@ -91,118 +88,113 @@ class Audio extends vkObserver {
 					text = `${ bitrate.kbps } кбит/с<span>${ fileSize } МБ</span>`;
 				}
 				let b = document.createElement('span');
+
 				b.className = 'bitrate';
 				b.innerHTML = text.replace('-', '');
-				audioContainer.appendChild(b);
+				target.appendChild(b);
 			}
 
 		})
 
 	}
 
-	setAudioUrl(options, e) {
+	setAudioUrl(target, options) {
 		const { id, title, duration } = options;
-		const rules = ['_audio_row', 'audio_row__inner'];
-		const matchedRules = rules.filter(rule => e.target.className.indexOf(rule) >= 0);
-		const isError = e.target.getAttribute('data-fetching-error');
-		const isAudio = matchedRules && matchedRules.length > 0;
-		const isClaimed = e.target.className.indexOf('claimed') >= 0;
-		const isDeleted = e.target.className.indexOf('audio_deleted') >= 0;
+		const isError = target.getAttribute('data-fetch-error');
+		const isFetching = target.getAttribute('data-fetching');
+		const downloadBtn = target.querySelector('.download-link');
+		const audioInfo = target.querySelector('.audio_row__inner');
 
-		const container = e.target.className.indexOf('audio_row__inner') >= 0
-							?
-							e.target
-							:
-							e.target.querySelector('.audio_row__inner');
+		if(isFetching) return;
 
-		const isFetching = container && container.getAttribute('data-fetching');
-		const downloadBtn = container && container.querySelector('.download-link');
-
-		if(isError || isFetching || isClaimed || isDeleted || downloadBtn) return;
-
-		if(isAudio && container) {
-			container.setAttribute('data-fetching', true);
-
-			const form = new FormData();
-
-			form.append('act', 'reload_audio');
-			form.append('al', '1');
-			form.append('ids', id);
-
-			xhr({
-				url: 'https://vk.com/al_audio.php',
-				method: 'POST',
-				body: form
-			})
-			.then(response => {
-				const filteredUrls = response.result
-										.split(',')
-										.filter(item => item.indexOf('mp3') >= 0);
-
-				const cleanUrl = filteredUrls[0].replace(/^"(.+(?="$))"$/, '$1');
-
-				return decodeURL(cleanUrl);
-			})
-			.then(url => {
-
-				if(url.indexOf('audio_api_unavailable') >= 0) {
-		 			throw new Error(url)
-				}
-
-				if(!downloadBtn) {
-					const d = document.createElement('a');
-
-					e.target.removeEventListener('mouseover', this.setAudioUrl, false);
-
-					d.className = 'download-link';
-					d.href = url;
-					d.setAttribute('download', title);
-					d.addEventListener('click', this.getblob, false);
-					container.insertBefore(d, container.firstChild);
-
-					options.url = url;
-					this.displayBitrate(e, options);
-				}
-
-
-			})
-			.catch(err => {
-				e.target.setAttribute('data-fetch-error', true);
-				// console.error('SET_AUDIO_URL', err, JSON.stringify(err))
-			})
-		}
+		target.setAttribute('data-fetching', true);
+		
+		const form = new FormData();
+		
+		form.append('act', 'reload_audio');
+		form.append('al', '1');
+		form.append('ids', id);
+					
+		xhr({
+			url: 'https://vk.com/al_audio.php',
+			method: 'POST',
+			body: form
+		})
+		.then(response => {
+			const filteredUrls = response.result
+									.split(',')
+									.filter(item => item.indexOf('mp3') >= 0);
+						
+			const cleanUrl = filteredUrls[0].replace(/^"(.+(?="$))"$/, '$1');
+		
+			return decodeURL(cleanUrl);
+		})
+		.then(url => {
+			let error = false;
+		
+			if(url.indexOf('audio_api_unavailable') >= 0) {
+				error = true;
+				target.removeAttribute('data-fetching');
+				target.setAttribute('data-fetch-error', true);
+			}
+		
+			if(!error && !downloadBtn) {
+				const d = document.createElement('a');
+		
+				target.setAttribute('data-fetched', true);
+		
+				d.className = 'download-link';
+				d.href = url;
+				d.setAttribute('download', title);
+				d.addEventListener('click', this.getblob, false);
+				audioInfo.insertBefore(d, audioInfo.firstChild);
+		
+				options.url = url;
+				this.displayBitrate(target, options);
+			}
+		
+		})
+		.catch(err => {
+			target.removeAttribute('data-fetching');
+			target.setAttribute('data-fetch-error', true);
+			//console.error('SET_AUDIO_URL', err, JSON.stringify(err))
+		})
 	}
 
 	showA(audios) {
 		let audioBlocks = audios || document.querySelectorAll('.audio_row');
 		audioBlocks = [].slice.call(audioBlocks);
 
-		function noBubbling(event) {
-			event.stopPropagation();
-		}
-
 		if (audioBlocks.length > 0) {
 			audioBlocks.forEach(audioBlock => {
 				const btn = audioBlock.querySelector('.audio_row_content');
-				// const btnPlay = btn.querySelector('.audio_row__inner');
 				const audioId = audioBlock.getAttribute('data-full-id');
 				const durationBlock = audioBlock.querySelector('.audio_row__duration').innerText;
 				const durationMinutes = durationBlock.split(':')[0];
 				const durationSeconds = durationBlock.split(':')[1];
 				const duration = (+durationMinutes * 60) + +durationSeconds;
 
-				// if (!btn.querySelector('.download-link') && linkContainer.indexOf('mp3') > 0) {
 				if (!btn.querySelector('.download-link')) {
+					const self = this;
 					const audioTitle = audioBlock.querySelector('.audio_row__title_inner').innerText;
 					const audioArtist = audioBlock.querySelector('.audio_row__performer').innerText;
 					const audioName = audioArtist + "-" + audioTitle;
 					const audioFullName = audioName.replace(/(<([^>]+)>)|([<>:"\/\\|?*.])/ig, '');
 					const options = {id: audioId, title: audioFullName, duration};
 
-					// audioBlock.addEventListener('mouseover', this.displayBitrate.bind(this), false);
 					audioBlock.addEventListener(
 						'mouseover',
-						this.setAudioUrl.bind(this, options),
+						function handler(e) {
+							const isClaimed = this.className.indexOf('claimed') >= 0;
+							const isDeleted = this.className.indexOf('audio_deleted') >= 0;
+							const isFetched = this.getAttribute('data-fetched');
+					
+							if(isFetched || isClaimed || isDeleted) {
+								this.removeEventListener('mouseover', handler, false);
+							}
+
+							self.setAudioUrl(this, options);
+						},
 						false
 					);
 
@@ -249,8 +241,6 @@ class Audio extends vkObserver {
 			}
 		})
 	}
-
-
 }
 
 export default Audio
