8bc246c7e1052f70a4d5951f0993cf8f16cfc979
Glebcha <glebcha@gmail.com>
2018-10-05 02:06:57 +0300

    removed login fix in favor of data-audio

--- source/js/app/app.js
+++ source/js/app/app.js
@@ -6,16 +6,3 @@ const watcher = new Watcher();
 
 vk.syncStorage();
 watcher.observe();
-
-chrome.runtime.onMessage.addListener(({query, details}, sender, sendResponse) => {
-  const pattern = new RegExp('queue_connection_events_queue', 'gi');
-
-  if (query === 'relogin') {
-    for (let key in localStorage) {
-      if (localStorage.hasOwnProperty(key) && key.match(pattern)) {
-        localStorage.setItem('VK_OBSERVER_ID', key.replace(/\D+/g, ''))
-        break;
-      }
-    }
-  }
-});
--- source/js/pages/background.js
+++ source/js/pages/background.js
@@ -1,23 +1,3 @@
-const storage = chrome.storage.sync;
-
-chrome.tabs.onUpdated.addListener((tabId, { status, url }, tab) => {
-  if (status && url && url.match(/\/login/g)) {
-    storage.set({notifyLogin: true});
-  }
-
-  storage.get('notifyLogin', ({notifyLogin}) => {
-    if (typeof notifyLogin === undefined) {
-      storage.set({notifyLogin: false})
-    }
-
-    if (notifyLogin && !tab.url.match(/\/login/g)) {
-      chrome.tabs.sendMessage(tabId, {query: 'relogin', details: notifyLogin});
-      storage.set({notifyLogin: false})
-    }
-  })
-  
-})
-
 chrome.runtime.onMessage.addListener((request) => {
   const {id, videoSrc, videoTitle} = request;
   const nameChunks = videoSrc.split('.');
